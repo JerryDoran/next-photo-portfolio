560807dc733d61066b369279726232a0a321ecf8
JerryDoran <jmdwebtechnologies@gmail.com>
2023-05-01 12:25:32 -0400

    1 binaries uploaded

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <main className="">
-      <p>Home</p>
+    <div className="h-screen bg-[url('https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG9ncmFwaHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60')">
+      <main className="">
+        <p>Home</p>
+      </main>
       <footer>Footer</footer>
-    </main>
+    </div>
   );
 }
