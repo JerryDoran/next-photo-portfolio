db6c6f89fd6766dea649b6825b426a0b5c062684
JerryDoran <jmdwebtechnologies@gmail.com>
2023-05-01 11:46:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# next-photo-portfolio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-ysuirr)
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="">
+      <p>Home</p>
+      <footer>Footer</footer>
+    </main>
+  );
+}
